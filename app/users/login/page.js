284ba23e14a144e2d0451e95bc02fa6d98a82198
s22002 <s22002@std.it-college.ac.jp>
2024-02-02 13:55:24 +0900

    ログイン画面、新規登録画面完了

--- app/users/login/page.js
+++ app/users/login/page.js
@@ -1,10 +1,103 @@
+"use client";
 
-const Home = () =>{
+import React, {useState, useEffect} from "react";
+import Link from "next/link";
+
+const LoginPage = () => {
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+    const [resMessages, setResMessages] = useState("")
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch("http://localhost:3030/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({name: name, pass: pass}),
+            });
+
+            if (response.ok) {
+                // ログイン成功
+                console.log("ログイン成功", response)
+                setResMessages("ログイン成功しました")
+                setName("")
+                setPass("")
+            } else {
+                // ログイン失敗
+                console.error("ログイン失敗", response);
+                setResMessages("ログイン失敗しました")
+                setName("")
+                setPass("")
+            }
+        } catch (error) {
+            console.error("error", error);
+        }
+    };
+
+    const handleLogout = async () => {
+        try {
+            const response = await fetch("http://localhost:3030/users/logout", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            if (response.ok) {
+                // ログアウト成功
+                console.log("ログアウトに成功", response)
+                setResMessages("ログアウトに成功しました")
+                setName("")
+                setPass("")
+            } else {
+                // ログアウト失敗
+                console.error("ログアウトに失敗", response);
+                setResMessages("ログアウトに失敗しました")
+                setName("")
+                setPass("")
+            }
+        } catch (error) {
+            console.error("error", error);
+            setName("")
+            setPass("")
+        }
+    };
+
+
     return (
         <>
-            a
+            <div className="container">
+                <header>
+                    <h1 className="display-4">Login</h1>
+                </header>
+                <form>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="name">NAME</label>
+                        <input className="form-control" id="name" type="text" value={name}
+                               onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">PASSWORD</label>
+                        <input className="form-control" type="password" value={pass}
+                               onChange={(e) => setPass(e.target.value)}/>
+                    </div>
+                    <button onClick={handleLogin} className="btn btn-primary" type="button">ログイン</button>
+                    <button onClick={handleLogout} className="btn btn-primary" type="button">ログアウト</button>
+                </form>
+                {resMessages ? (
+                    <p className="text-danger">{resMessages}</p>
+                ) : (
+                    <></>
+                )}
+                <div className="mt-4">
+                    <a href="./singnup">新規登録ページへ&gt;&gt;</a>
+                </div>
+            </div>
         </>
-    );
-}
 
-export default Home
+    );
+};
+
+export default LoginPage;
---
+++ app/users/singnup/page.js
@@ -0,0 +1,69 @@
+"use client";
+
+import React, {useState, useEffect} from "react";
+
+
+const SingUpPage = () => {
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+    const [resMessages, setResMessages] = useState("")
+
+
+    const handleSignup = async () => {
+        try {
+            const response = await fetch("http://localhost:3030/users/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ name, pass }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log("新規登録完了:", data.message);
+                setResMessages("ユーザー登録をしました")
+            } else {
+                console.error("新規登録失敗", data.message);
+                setResMessages("ユーザー登録を失敗しました")
+
+            }
+        } catch (error) {
+            console.error("error", error);
+        }
+    };
+
+    return (
+        <>
+            <div className="container">
+                <header>
+                    <h1 className="display-4">SingUp</h1>
+                </header>
+                <form>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="name">NAME</label>
+                        <input className="form-control" id="name" type="text" value={name}
+                               onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">PASSWORD</label>
+                        <input className="form-control" type="password" value={pass}
+                               onChange={(e) => setPass(e.target.value)}/>
+                    </div>
+                    <button onClick={handleSignup} className="btn btn-primary" type="button">新規登録</button>
+                </form>
+                {resMessages ? (
+                    <p className="text-danger">{resMessages}</p>
+                ) : (
+                    <></>
+                )}
+                <div className="mt-4">
+                    <a href="./login">&lt;&lt;ログインページへ</a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SingUpPage;
